a56afe179f6ff8253a4fd3bffc3288ea05cc78e7
dkme01 <dkme01@gmail.com>
2021-04-07 18:24:42 -0300

    LIstagem de repositórios

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,9 +1,9 @@
 export function RepositoryItem({ repository }) {
   return (
     <li>
-      <strong>{repository.name ?? "Default"}</strong>
-      <p>{repository.description ?? "Default Repository"}</p>
-      <a href={repository.link ?? "#"}>Acessar repositório</a>
+      <strong>{repository.name}</strong>
+      <p>{repository.description}</p>
+      <a href={repository.html_url}>Acessar repositório</a>
     </li>
   );
 }
