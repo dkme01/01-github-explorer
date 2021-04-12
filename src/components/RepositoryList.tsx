341d73d235cf52dd10f931044e5d1172a11707cf
dkme01 <dkme01@gmail.com>
2021-04-12 16:09:38 -0300

    changed file extensions created interfaces for repository

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,10 +3,15 @@ import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
 import { useState, useEffect } from "react";
 
-// https://api.github.com/orgs/rocketseat/repos
+interface Repository {
+  id: number;
+  name: string;
+  description: string;
+  html_url: string;
+}
 
 export default function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Array<Repository>>([]);
 
   useEffect(() => {
     fetch("https://api.github.com/orgs/rocketseat/repos")
